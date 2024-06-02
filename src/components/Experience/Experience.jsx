e3e8a94321d8d9bc5fd5670393b25abeec3fa662
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-06-02 14:45:19 +0530

    Updated

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./src/components/Experience/Experience.css";
-import gif from "./src/components/assets/coming-soon-button-sliding-mobile-art-oeva2rqfq38o1vd0.gif";
-import Nav from "./src/components/Nav/Nav.jsx";
+import gif from "./components/assets/coming-soon-button-sliding-mobile-art-oeva2rqfq38o1vd0.gif";
+import Nav from "./components/Nav/Nav.jsx";
 function Experience(){
     return(
         <>
