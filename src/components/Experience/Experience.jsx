8ce093b60e0aa4b527270b9b0ee825b3e55d8bae
Noorain464 <146834328+Noorain464@users.noreply.github.com>
2024-06-02 14:06:18 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "/home/syeda/Desktop/Assignment-2/assignment-2/src/components/Experience/Experience.css";
-import gif from "/home/syeda/Desktop/Assignment-2/assignment-2/src/components/assets/coming-soon-button-sliding-mobile-art-oeva2rqfq38o1vd0.gif";
-import Nav from "/home/syeda/Desktop/Assignment-2/assignment-2/src/components/Nav/Nav.jsx";
+import "./src/components/Experience/Experience.css";
+import gif from "./src/components/assets/coming-soon-button-sliding-mobile-art-oeva2rqfq38o1vd0.gif";
+import Nav from "./src/components/Nav/Nav.jsx";
 function Experience(){
     return(
         <>
