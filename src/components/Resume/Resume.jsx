f89da7515b82745bd42fae1b1d3305e429ccc3d7
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-06-02 13:45:00 +0530

    resume

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+function Resume(){
+    return(
+        <>
+        <Nav />
+        <div className="Resume-container">
+            <img src={gif} alt="" className="Resume"/>
+        </div>
+        </>
+    )
+}
+export default Resume;
